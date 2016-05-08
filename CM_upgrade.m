71296189f8882a97c63a3240192fa98c5b29034c
FilipJoh <filip.johannesson@gmail.com>
2016-05-08 20:06:44 +0200

    refined ignore file

--- CM_upgrade.m
+++ CM_upgrade.m
@@ -17,21 +17,20 @@ T = 1 / F_s;
 %% TEST
 
 %% Human
-ecg_p=ecg_h;
-t=t_h;
-nbrOfBeats=find(t<length(ecg_p),1,'last')-1;
-threshold=1;
+% ecg_p=ecg_h;
+% t=t_h;
+% nbrOfBeats=find(t<length(ecg_p),1,'last')-1;
+
 %% Pig one
 % ecg_p=ecg_p1;
 % t=t_p1;
 % nbrOfBeats=length(t)-2;
-% threshold=1e-3;
+
 
 %% Pig two
-% ecg_p=ecg_p2;
-% t=t_p2;
-% nbrOfBeats=length(t)-2; 
-% threshold=0.01;
+ecg_p=ecg_p2;
+t=t_p2;
+nbrOfBeats=length(t)-2; 
 
 % create a lowpass filter
 cutFreq = 50; %Hz
@@ -50,84 +49,70 @@ maxBeatDuration=max(beatDuration)+1;
 twave_beg = t(1) + (0.04 / T);
 twave_end = twave_beg + (0.35 / T);
 TwindowLength=floor((0.4*sqrt(meanDuration))/T);
-tempSignalMat=zeros(128,TwindowLength);
+tempSignalMat=zeros(nbrOfBeats,TwindowLength);
 signalMat=zeros(nbrOfBeats,TwindowLength);%zeros(nbrOfBeats,twave_end-twave_beg);
-heartBeatMatrix=cell(nbrOfBeats,1);
-starts=zeros(128,1);
-%figure;
-%0.23
+heartBeatCell=cell(nbrOfBeats,1);
+starts=zeros(nbrOfBeats,1);
+
 figure;
 for i=1:size(ecg_p,1)
     ecg=ecg_p(i,:);
     for a=1:nbrOfBeats
-        heartBeatMatrix{a}=ecg(t(a):t(a+1)); 
+        heartBeatCell{a}=ecg(t(a):t(a+1)); 
     end
     
-    for a = 1:(nbrOfBeats-127)
-        for v=1:128
-            if beatDuration(a)*T<0.6
-                startoffset=0.06;
-            elseif beatDuration(a)*T<1.1
-                startoffset=0.1;    
-            else
-                startoffset=0.15;
-            end
-                    
-            twave_beg =(startoffset / T);
-            %twave_end = twave_beg + ((0.35-startoffset) / T);
-            %temp = ecg(twave_beg:twave_end);
-            tempSignalMat(v,:)=heartBeatMatrix{a+v-1}(twave_beg:(twave_beg-1+TwindowLength));
-            starts(v)=twave_beg;
+    for v=1:nbrOfBeats
+        if beatDuration(a)*T<0.6
+            startoffset=0.06;
+        elseif beatDuration(a)*T<1.1
+            startoffset=0.1;    
+        else
+            startoffset=0.15;
         end
-        TempMed=median(tempSignalMat);
-        for v=1:128
-            Best=0;
-            bestStart=starts(v);
-            for u=-0.03:0.001:0.03
-                offsetVal=TempMed*(heartBeatMatrix{v}((starts(v)+floor(u/T)):(starts(v)+floor(u/T)-1+TwindowLength)))';
-                if offsetVal>Best;
-                    Best=offsetVal;
-                    bestStart=starts(v)+u/T;
-                end    
-            end
-            signalMat(v+a-1,:)=heartBeatMatrix{v+a-1}(bestStart:(bestStart-1+TwindowLength));
-        end
+
+        twave_beg =(startoffset / T);
+        tempSignalMat(v,:)=heartBeatCell{v}(twave_beg:(twave_beg-1+TwindowLength));
+        starts(v)=twave_beg;
     end
-% create bandstopfilter in order to sort of respiratory signal
+    TempMed=median(tempSignalMat);
+    for v=1:nbrOfBeats
+        Best=0;
+        bestStart=starts(v);
+        for u=-0.03/T:0.03/T
+            offsetVal=TempMed*(heartBeatCell{v}((starts(v)+floor(u)):(starts(v)+floor(u)-1+TwindowLength)))';
+            if offsetVal>Best;
+                Best=offsetVal;
+                bestStart=starts(v)+u;
+            end    
+        end
+        signalMat(v,:)=heartBeatCell{v}(bestStart:(bestStart-1+TwindowLength));
+    end
+
 [n,Wn]=buttord([0.11 0.38],[0.14 0.35],3,60);
 [B,A]=butter(n,Wn,'stop');
-% fvtool(B,A);
 
 for a=1:size(nbrOfBeats)
     y=filtfilt(B,A,signalMat(a,:)); 
 end
     
-    
-%detextion    
-    window=7;
-%     ACI=zeros(window,1);
-%     ACM=zeros(window,1);
-    
+   
+%detection    
+    window=7;   
     ACI=zeros(nbrOfBeats,1);
     ACM=zeros(nbrOfBeats,1);
     ACMmatrix=[];
     startWin=1;
     endWin=startWin+window-1;
     alt=zeros(size(signalMat,1),1);
-    %figure;
+
     while endWin~=size(signalMat,1);
         Tmdn=median(signalMat(startWin:endWin,:));
-        for j=startWin:endWin%1:window     
-               % ACI(j)=max(xcorr(signalMat(startWin-1+j,:),Tmdn))./max(xcorr(Tmdn,Tmdn));%signalMat(a,:)*Tmdn'./(Tmdn*Tmdn');
-                %ACM(j)=2*abs(ACI(j)-1)*sum(Tmdn.^2)./sum(abs(Tmdn));
+        for j=startWin:endWin     
                 [acf,lags,bound]=autocorr(Tmdn);
-                ACI(j)=max(xcorr(signalMat(j,:),Tmdn))/max(xcorr(Tmdn,Tmdn));
-                %ACI(j)=sum(signalMat(j,:).*Tmdn)/(sum(Tmdn.^2));%max(xcorr(signalMat(j,:),Tmdn))./max(xcorr(Tmdn,Tmdn));%signalMat(a,:)*Tmdn'./(Tmdn*Tmdn');
+                ACI(j)=max(xcorr(signalMat(j,:),Tmdn))/max(xcorr(Tmdn,Tmdn)); %ACI(j)=sum(signalMat(j,:).*Tmdn)/(sum(Tmdn.^2));%max(xcorr(signalMat(j,:),Tmdn))./max(xcorr(Tmdn,Tmdn));%signalMat(a,:)*Tmdn'./(Tmdn*Tmdn');
                 ACM(j)=2*abs(ACI(j)-1)*sum(Tmdn.^2)./sum(abs(Tmdn));
         end
         ACMmatrix=[ACMmatrix ACM];
-%         plot(1:length(ACI),ACI-1)
-%         axis([0 nbrOfBeats -1 1])
         alt(startWin:endWin)=or(alt(startWin:endWin),detectAlt(ACI(startWin:endWin)-1));%mean(ACM(startWin:endWin));
         ACM=zeros(nbrOfBeats,1);
         ACI=zeros(nbrOfBeats,1);
@@ -141,38 +126,27 @@ end
            subplot(3,4,i)
     end
     ACMcomp=zeros(length(ACI),1);
-    %figure; 
-    plot(1:length(alt),alt,'LineWidth',2);
+    
+ 
+    %figure;
+    samples=1:length(ACMcomp);
+    ACItime=t(find(alt))*T/60;
+    time=t(1:length(ACMcomp))*T/60; %minutes
+    
     for j=1:size(ACM,1)
         if alt(j)==1
             ACMcomp(j)=mean(ACMmatrix(j,find(ACMmatrix(j,:))));
         end
     end
     hold on;
-    plot(1:length(ACMcomp),ACMcomp,'LineWidth',1)
+    plot(time,ACMcomp,'r','LineWidth',1)
+    hold on;
+    plot(ACItime,zeros(length(ACItime),1),'b.','LineWidth',5);
+    
     %figure;
     %contourf(ACMmatrix,'EdgeColor','none')
-    title(['ECG-lead no: ' num2str(i)])
-    xlabel('heartbeats')
-    ylabel('TWA amplitude (mv)')
-%     figure;
-%     plot((1:length(temp))*T,temp);
-
-  
-    % figure;
-    % plot(1:length(Tmdn),Tmdn)
-
-
-
-    %[B,A]=butter(16,0.5,'high');
-    %ACI=filter(B,A,ACI);
-
-    
-    
-%     ACM=medfilt1(ACM,30);
-
-%     figure;
-%     if(size(ecg_p,1)>1)
-%            subplot(size(ecg_p,1)/2,2,i)
-%     end
+    title(['ECG lead no: ' num2str(i)])
+    xlabel('Time /minutes')
+    ylabel('TWA amplitude  /\muv')
+    legend('TWA estimate','Binary mask')
 end
